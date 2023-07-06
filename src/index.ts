257559313c1eee3ae3d3fb09ecd47a382fa91210
Qin Guan <qinguan20040914@gmail.com>
2023-07-06 10:43:34 +0800

    feat: table cell align

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import { Fragment, jsx, jsxDEV } from 'vue/jsx-runtime'
 import { toJsxRuntime } from 'hast-util-to-jsx-runtime'
 
+// @ts-expect-error: no declaration files
+import tableCellStyle from '@mapbox/hast-util-table-cell-style'
+
 import type { VNode } from 'vue'
 import type { Root } from 'hast'
 import type { Plugin, Processor } from 'unified'
 
-import type { ComponentsWithoutNodeOptions } from './complex-types'
+import type { ComponentsWithNodeOptions, ComponentsWithoutNodeOptions } from './complex-types'
 
-export type Options = ComponentsWithoutNodeOptions
+interface SharedOptions {
+  fixTableCellAlign?: boolean
+}
+
+export type Options = SharedOptions & (ComponentsWithNodeOptions | ComponentsWithoutNodeOptions)
 
 export const rehypeVue: Plugin<[(Options | undefined)?], Root, VNode> = function (this: Processor, options) {
-  this.Compiler = (node) => {
+  this.Compiler = (_node) => {
+    let node = _node
+    if (options?.fixTableCellAlign)
+      node = tableCellStyle(node)
+
     // @ts-expect-error this works...
     return toJsxRuntime(node, {
       Fragment,
